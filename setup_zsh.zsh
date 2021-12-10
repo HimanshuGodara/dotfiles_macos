a38185248ffb19c91902ca744f2443e860690bdf
Himanshu Godara <6560138+HimanshuGodara@users.noreply.github.com>
2021-12-10 19:12:25 +0530

    Automated the path of zsh using brew --prefix

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting ZSH setup >>>\n"
 # Installation unnecessary, it's in the 
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+brew --prefix zsh | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/opt/homebrew/bin/zsh'
