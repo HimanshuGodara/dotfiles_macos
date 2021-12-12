95e1b546204e3c4176a22061b3c3f9b2f22a20e0
Himanshu Godara <himanshugodara23@gmail.com>
2021-12-12 16:18:38 +0530

    Symlink sh to zsh
    Additional smaller changes:- 
    - Spelling correction in setup_homebrew
    - Removed shebang.zsh

--- shebang.zsh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env zsh
-echo "\n <<< Running $0 >>> \n"
-echo "ZSH_Version = $ZSH_VERSION" 
-echo "The current shell is: $(ps $$ -ocomm=)"
-exists brew && echo "brew is installed" || echo "brew is not installed" 
-exists nonexistent && echo "nonexistent is installed" || echo "nonexistent is not installed"
-which exists
