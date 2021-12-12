c633946e5a5bf7f24e48ff54c6dc4dd94047f3a7
Himanshu Godara <6560138+HimanshuGodara@users.noreply.github.com>
2021-12-12 19:10:58 +0530

    Install trash-cli and alias rm to trash

--- setup_node.zsh
+++ setup_node.zsh
@@ -19,5 +19,6 @@ npm install --global @ionic/cli
 npm install --global typescript
 npm install --global json-server
 npm install --global http-server
+npm install --global trash-cli
 echo "Global NPM packages installed:"
 npm list --global --depth=0
